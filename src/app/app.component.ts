2214e861bd8ec9d5ef8d2ce8cc164b6f4f1ed5da
danmalmx <danmalmx@gmail.com>
2019-10-17 16:59:02 +0200

    Basic calendar setup completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,24 @@
+import { Component } from '@angular/core';
 import { Calendar } from '@fullcalendar/core';
-import { DayGrid } from '@fullcalendar/daygrid';
-
+import dayGridPlugin from '@fullcalendar/daygrid';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MintTvätt';
+  title = 'MinTvätt';
+  calendarPlugins = [dayGridPlugin];
+
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
+// document.addEventListener('DOMContentLoaded', function() {
+//   var calendarEl = document.getElementById('calendar');
 
-let calendar = new Calendar(calendarEl, {
-    plugins: [ DayGrid ],
-    defaultView: 'dayGridMonth'
-  });
+// let calendar = new Calendar(calendarEl, {
+//     plugins: [ DayGrid ],
+//     defaultView: 'dayGridMonth'
+//   });
 
-   calendar.render();
-});
+//    calendar.render();
+// });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FullCalendarModule } from "@fullcalendar/angular";
-
+import { FullCalendarModule } from '@fullcalendar/angular'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
