6777754ef2956951916fbc1f66e7c2424f0b7e29
danmalmx <danmalmx@gmail.com>
2019-11-20 19:49:39 +0100

    adding day view and resource librabry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { Component } from '@angular/core';
-import { Calendar } from '@fullcalendar/core';
+// import { Calendar } from '@fullcalendar/angular/fullcalendar-angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MinTvätt';
+  title = 'MinTvättTid';
   calendarPlugins = [dayGridPlugin];
-
+  resources: [];
 }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var calendarEl = document.getElementById('calendar');
-
-// let calendar = new Calendar(calendarEl, {
-//     plugins: [ DayGrid ],
-//     defaultView: 'dayGridMonth'
-//   });
-
-//    calendar.render();
-// });
